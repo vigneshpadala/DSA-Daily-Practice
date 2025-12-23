6479a7865d1a605c3ee07dc6a3270cbada204d6d
vigneshpadala <vigneshpadala2003@gmail.com>
2025-12-23 19:07:36 +0530

    Add Array.py with basic array operations in Python
    Added a Python script demonstrating basic array operations, including creation, accessing elements, and iteration.

---
+++ Arrays/Day-01/Array.py
@@ -0,0 +1,23 @@
+## Day 01 – DSA Learning
+
+- Topic: Arrays
+- What I learned:
+- Basics of arrays
+- Accessing elements using index
+- Iterating through array elements
+- Language: Python
+
+
+# Creating an array (list in Python)
+arr = [10, 20, 30, 40, 50]
+
+print("Array:", arr)
+
+# Accessing elements using index
+print("First element:", arr[0])     # Index 0
+print("Third element:", arr[2])     # Index 2
+print("Last element:", arr[-1])     # Last element
+
+# Iterating using for loop
+for element in arr:
+    print(element)
