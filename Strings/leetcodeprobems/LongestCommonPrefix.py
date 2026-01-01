0ae02d8340d5bf0599afeae379c6c5a28a32dded
vigneshpadala <vigneshpadala2003@gmail.com>
2026-01-01 13:19:14 +0530

    Add LongestCommonPrefix solution in Python
    Implement solution for the Longest Common Prefix problem.

---
+++ Strings/leetcodeprobems/LongestCommonPrefix.py
@@ -0,0 +1,17 @@
+#14. Longest Common Prefix
+link:https://leetcode.com/problems/longest-common-prefix/description/
+
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        if not strs:
+            return ""
+        
+        prefix = strs[0]
+
+        for i in range(1, len(strs)):
+            while not strs[i].startswith(prefix):
+                prefix = prefix[:-1]
+                if not prefix:
+                    return ""
+        
+        return prefix
