e22c37a78fa0ac2c88da19b2ec929d6e874aab58
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-01-28 19:35:45 +0530

    Create 20. Valid Parentheses.py

---
+++ Stack/Leetcode Stack  problems/20. Valid Parentheses.py
@@ -0,0 +1,17 @@
+# 20. Valid Parentheses
+
+class Solution(object):
+    def isValid(self, s):
+        stack=[]
+        pairs={')':'(',']':'[','}':'{'}
+
+        for ch in s:
+            if ch in pairs:
+                if not stack or stack[-1]!=pairs[ch]:
+                    return False
+
+                stack.pop()
+            else:
+                stack.append(ch)
+            
+        return not stack
