e62c26d2dc80220ecbc2dcd773d5e3603b5d73bf
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-02-13 20:39:54 +0530

    Add multiple reverse array implementations
    Refactor reverse array function to include multiple implementations with different time complexities.

--- Arrays/Day-02/Reverse Array.py
+++ Arrays/Day-02/Reverse Array.py
@@ -1,4 +1,15 @@
-# brute 
+# brute - 0 (n)
+def reversearray(arr):
+    n=len(arr)
+    for i in range(n//2):
+        arr[i],arr[n-1-i]=arr[n-1-i],arr[i]
+    return arr
+
+arr=[1,2,3,4,5]
+print(reversearray(arr))
+
+
+# avg - O(log n)
 def reversearray(arr):
     l=0
     r=len(arr)-1
@@ -8,15 +19,13 @@ def reversearray(arr):
         r-=1
     return arr
 
-
 arr=[1,2,3,4,5]
 print(reversearray(arr))
 
 
-# optimal
+# optimal - O(1)
 def reversearray(arr):
     return arr[::-1]
-
 
 arr=[1,2,3,4,5]
 print(reversearray(arr))
