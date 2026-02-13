72756d542c6b6f3b8fdbe75c61035840efbf5707
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-02-13 20:50:54 +0530

    Refactor remove duplicate function for optimization
    Refactored remove duplicate function to use set for optimal performance.

--- Arrays/Day-03/remove duplicate.py
+++ Arrays/Day-03/remove duplicate.py
@@ -1,13 +1,17 @@
-# Brute
+# Brute - O(n2)
 def removeduplicate(arr):
     unq=[]
     for num in arr:
         if num  not in unq:
             unq.append(num)
-        else:
-            num+=1
     return unq
 
 arr=[1,2,2,3,3,4,4,5]
 print(removeduplicate(arr))
 
+# optimal - O(n)
+def removeduplicate(arr):
+    return list(set(arr))
+
+arr=[1,2,2,3,3,4,4,5]
+print(removeduplicate(arr))
