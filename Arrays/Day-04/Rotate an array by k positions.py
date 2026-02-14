b8671c50695e03fa9827dbf1844d3b0166265cd8
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-02-14 13:43:11 +0530

    Implement brute and optimal methods for array rotation
    Added an optimal function for rotating an array.

--- Arrays/Day-04/Rotate an array by k positions.py
+++ Arrays/Day-04/Rotate an array by k positions.py
@@ -1,4 +1,5 @@
 #  Rotate an array by k positions
+# brute - O(n*(k%n))
 def brute(arr,k):
     n=len(arr)
     rotations= k % n
@@ -6,8 +7,17 @@ def brute(arr,k):
         last_ele = arr.pop()
         arr.insert(0,last_ele)
     return arr
-    
-    
+       
 arr=[1,2,3,4,5]
 k=2
 print(brute(arr,k))
+
+# optimal- O(n)
+def optimal(arr,k):
+    k = k % len(arr)
+    print(f'k={k},arr[-k:]={arr[-k:]},arr[:-k]={arr[:-k]}')
+    return arr[-k : ] + arr[ : - k] 
+        
+arr=[1,2,3,4,5]
+k=3
+print(optimal(arr,k))
