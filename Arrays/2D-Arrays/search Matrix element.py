d83ceae996304735dbbe7db6d8d05349123b5ec9
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-02-16 15:42:30 +0530

    Update search Matrix element.py

--- Arrays/2D-Arrays/search Matrix element.py
+++ Arrays/2D-Arrays/search Matrix element.py
@@ -1,3 +1,4 @@
+# leetcode problem 74
 # search Matrix element they  or not
 
 class Solution(object):
@@ -6,4 +7,28 @@ class Solution(object):
             for col in row:
                 if target == col:
                     return True
+        return False
+
+# using binary search
+
+class Solution(object):
+    def searchMatrix(self, matrix, target):
+        rows=len(matrix)
+        cols=len(matrix[0])
+
+        l=0
+        h=(rows*cols)-1
+
+        while l<=h:
+            mid = (l+h)//2
+            row=mid//cols
+            col=mid%cols
+            middle_value=matrix[row][col]
+
+            if middle_value==target:
+                return True
+            elif middle_value<target:
+                l=mid+1
+            else:
+                h=mid-1
         return False
