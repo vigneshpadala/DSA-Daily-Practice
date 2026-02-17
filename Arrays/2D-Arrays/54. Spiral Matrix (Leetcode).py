dfda8dcb9133fcc185a71e677616c21980742a9d
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-02-17 19:25:10 +0530

    Create 54. Spiral Matrix (Leetcode).py

---
+++ Arrays/2D-Arrays/54. Spiral Matrix (Leetcode).py
@@ -0,0 +1,16 @@
+# 54. Spiral Matrix
+
+class Solution(object):
+    def spiralOrder(self, matrix):
+        res=[]
+        while matrix:
+            res+=matrix.pop(0)
+            if matrix and matrix[0]:
+                for row in matrix:
+                    res.append(row.pop())
+            if matrix:
+                res+=matrix.pop()[::-1]
+            for row in reversed(matrix):
+                if row:
+                    res.append(row.pop(0))
+        return res
