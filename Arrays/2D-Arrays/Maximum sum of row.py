a547c801d370d7d7d2baeaa387c83a3dcaddf20c
VIGNESHPADALA <vigneshpadala2003@gmail.com>
2026-02-17 18:54:31 +0530

    Create Maximum sum of row.py

---
+++ Arrays/2D-Arrays/Maximum sum of row.py
@@ -0,0 +1,17 @@
+# Maximum sum of row
+def maximum(arr):
+    max_sum=float("-inf")
+    max_ind=-1
+    for i in range(len(arr)):
+        row_sum=sum(arr[i])
+        if row_sum>max_sum:
+            max_sum=row_sum
+            max_ind=i
+    print("max sum:",max_sum)
+    print("row:",max_ind)
+    
+    
+arr=[[1,2,3],[4,5,6],[7,8,9]]
+print(maximum(arr))
+
+# one line code 
